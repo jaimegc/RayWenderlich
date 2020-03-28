14c91f66bd753b3e04b85abc07f754ebbec4424b
Jaime Guerrero Cubero <jguerrero@carto.com>
2020-03-28 19:01:52 +0100

    Finished tutorial

--- ViewBindingTutorialForAndroid-GettingStarted/app/src/main/java/com/raywenderlich/android/bmicalc/profile/ProfileActivity.kt
+++ ViewBindingTutorialForAndroid-GettingStarted/app/src/main/java/com/raywenderlich/android/bmicalc/profile/ProfileActivity.kt
@@ -40,6 +40,7 @@ import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.raywenderlich.android.bmicalc.BMIApplication
 import com.raywenderlich.android.bmicalc.R
+import com.raywenderlich.android.bmicalc.databinding.ActivityProfileBinding
 import com.raywenderlich.android.bmicalc.model.Person
 import com.raywenderlich.android.bmicalc.profile.viewmodel.ProfileViewModel
 import com.raywenderlich.android.bmicalc.profile.viewmodel.ProfileViewModelFactory
@@ -50,9 +51,7 @@ class ProfileActivity : AppCompatActivity() {
 
   private lateinit var viewModel: ProfileViewModel
 
-  private lateinit var editTextBirthdate: EditText
-  private lateinit var editTextHeight: EditText
-  private lateinit var fab: View
+  private lateinit var binding: ActivityProfileBinding
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
@@ -60,14 +59,11 @@ class ProfileActivity : AppCompatActivity() {
     val factory = ProfileViewModelFactory((application as BMIApplication).repository)
     viewModel = ViewModelProviders.of(this, factory)[ProfileViewModel::class.java]
 
-    setContentView(R.layout.activity_profile)
+    binding = ActivityProfileBinding.inflate(layoutInflater)
+    setContentView(binding.root)
 
-    editTextBirthdate = findViewById(R.id.editText_birthdate)
-    editTextHeight = findViewById(R.id.editText_height)
-    fab = findViewById(R.id.fab)
-
-    editTextBirthdate.setOnClickListener {
-      showDatePickerDialog(editTextBirthdate)
+    binding.editTextBirthdate.setOnClickListener {
+      showDatePickerDialog(binding.editTextBirthdate)
     }
 
     viewModel.loadProfile()
@@ -86,12 +82,12 @@ class ProfileActivity : AppCompatActivity() {
 
   private fun showProfile(person: Person?) {
     person?.let {
-      editTextBirthdate.setText(it.birthdate.toFormattedString())
-      editTextHeight.setText(it.height.toFormattedString())
+      binding.editTextBirthdate.setText(it.birthdate.toFormattedString())
+      binding.editTextHeight.setText(it.height.toFormattedString())
     }
 
-    fab.setOnClickListener {
-      viewModel.saveProfile(editTextBirthdate.text.toString(), editTextHeight.text.toString())
+    binding.fab.setOnClickListener {
+      viewModel.saveProfile(binding.editTextBirthdate.text.toString(), binding.editTextHeight.text.toString())
     }
   }
 }
